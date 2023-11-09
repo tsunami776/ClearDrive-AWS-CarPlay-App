3fc2abdfb9cb3d323286d826b071abfc4b361b22
tsunami776 <43768723+tsunami776@users.noreply.github.com>
2023-11-08 19:57:53 -0600

    Live message sent via AppSync when entering geofence

--- mobile/mobile/Views/SettingsView.swift
+++ mobile/mobile/Views/SettingsView.swift
@@ -14,13 +14,13 @@ struct SettingsView: View {
     var latitude: Double
     var longitude: Double
     
-    let geofencePolygonCoordinates: [CLLocationCoordinate2D] = [
-            CLLocationCoordinate2D(latitude: 37.335034875313355, longitude: -122.06030576013998),
-            CLLocationCoordinate2D(latitude: 37.32956891898867, longitude: -122.06030576013998),
-            CLLocationCoordinate2D(latitude: 37.32956891898867, longitude: -122.04902156819315),
-            CLLocationCoordinate2D(latitude: 37.335034875313355, longitude: -122.04902156819315),
-            CLLocationCoordinate2D(latitude: 37.335034875313355, longitude: -122.06030576013998) // Close the polygon by repeating the first point
-        ]
+//    let geofencePolygonCoordinates: [CLLocationCoordinate2D] = [
+//            CLLocationCoordinate2D(latitude: 37.335034875313355, longitude: -122.06030576013998),
+//            CLLocationCoordinate2D(latitude: 37.32956891898867, longitude: -122.06030576013998),
+//            CLLocationCoordinate2D(latitude: 37.32956891898867, longitude: -122.04902156819315),
+//            CLLocationCoordinate2D(latitude: 37.335034875313355, longitude: -122.04902156819315),
+//            CLLocationCoordinate2D(latitude: 37.335034875313355, longitude: -122.06030576013998) // Close the polygon by repeating the first point
+//        ]
 
     
     @State private var weatherAlerts = false
@@ -45,19 +45,19 @@ struct SettingsView: View {
                     }
                 }
             }
-            FormButton(label: "Check Geofence", action: {
-                    let currentCoordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-                    if isCoordinate(currentCoordinate, insidePolygon: geofencePolygonCoordinates) {
-                        // Call the mutation query here if inside the geofence
-                        Task {
-                            await checkGeofenceAndSendMessage()
-                        }
-                        print("inside geofence.")
-                    }else {
-                        print("outside geofence")
-                    }
-                    showSettings.toggle()
-                })
+//            FormButton(label: "Check Geofence", action: {
+//                    let currentCoordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+//                    if isCoordinate(currentCoordinate, insidePolygon: geofencePolygonCoordinates) {
+//                        // Call the mutation query here if inside the geofence
+//                        Task {
+//                            await checkGeofenceAndSendMessage()
+//                        }
+//                        print("inside geofence.")
+//                    }else {
+//                        print("outside geofence")
+//                    }
+//                    showSettings.toggle()
+//                })
             
             Spacer()
             FormButton(label: "Dismiss", action: {showSettings.toggle()})
@@ -65,24 +65,24 @@ struct SettingsView: View {
     }
 }
 
-func isCoordinate(_ coordinate: CLLocationCoordinate2D, insidePolygon polygonCoordinates: [CLLocationCoordinate2D]) -> Bool {
-    let polygon = MKPolygon(coordinates: polygonCoordinates, count: polygonCoordinates.count)
-    let mapPoint = MKMapPoint(coordinate)
-    
-    let polygonRenderer = MKPolygonRenderer(polygon: polygon)
-    let polygonViewPoint = polygonRenderer.point(for: mapPoint)
-    return polygonRenderer.path.contains(polygonViewPoint)
-}
-
-func checkGeofenceAndSendMessage() async {
-    do {
-        let timestamp = ISO8601DateFormatter().string(from: Date())
-        let result = try await DataService().createVehicleMessage(message: "inside geofence.", owner: "Vehicle1", timestamp: timestamp)
-        print(result)
-    }catch {
-        print("Error fetching places: \(error)")
-    }
-}
+//func isCoordinate(_ coordinate: CLLocationCoordinate2D, insidePolygon polygonCoordinates: [CLLocationCoordinate2D]) -> Bool {
+//    let polygon = MKPolygon(coordinates: polygonCoordinates, count: polygonCoordinates.count)
+//    let mapPoint = MKMapPoint(coordinate)
+//
+//    let polygonRenderer = MKPolygonRenderer(polygon: polygon)
+//    let polygonViewPoint = polygonRenderer.point(for: mapPoint)
+//    return polygonRenderer.path.contains(polygonViewPoint)
+//}
+//
+//func checkGeofenceAndSendMessage() async {
+//    do {
+//        let timestamp = ISO8601DateFormatter().string(from: Date())
+//        let result = try await DataService().createVehicleMessage(message: "Entered geofence.", owner: "Vehicle1", timestamp: timestamp)
+//        print(result)
+//    }catch {
+//        print("Error fetching places: \(error)")
+//    }
+//}
 
 //struct SettingsView_Previews: PreviewProvider {
 //    static var previews: some View {
